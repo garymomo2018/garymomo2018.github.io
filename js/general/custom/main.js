43646e8058a7a889e051e7bb14066d4ff4e48702
garymomo2018 <garymomo2018@gmail.com>
2018-05-30 14:50:11 +0800

    2018/05/30 雛型

---
+++ js/general/custom/main.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function() {
+	var width = window.innerWidth;
+	var ori_width = "0";
+
+	resize();
+	$(window).on("resize", resize);
+
+	function resize() {
+		width = window.innerWidth;
+
+		if (width != ori_width) {
+			if (width > 767) {
+				$(".container-fluid").css("padding-right", "0px");
+				$(".container-fluid").css("padding-left", "0px");
+			} else {
+				$(".container-fluid").css("padding-right", "15px");
+				$(".container-fluid").css("padding-left", "15px");
+			}
+		}
+		ori_width = width;
+	}
+});
