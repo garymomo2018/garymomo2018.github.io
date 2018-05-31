935e8f8fffc06b382f9f5a4c09f22dd31799c32b
garymomo2018 <garymomo2018@gmail.com>
2018-05-31 11:56:05 +0800

    2018/05/31 修正版面大小配置

--- js/general/custom/main.js
+++ js/general/custom/main.js
@@ -7,8 +7,13 @@
 
 	function resize() {
 		width = window.innerWidth;
-		height = $(document).innerHeight;
-		$(".mainContain").css("height", height);
+
+		$(".mainContain").css("height","auto");
+		if($(window).height() > $(".mainContain").css("height").substring(0, $(".mainContain").css("height").indexOf("px"))) {
+			var setheight = $(window).height() - 71;
+			$(".mainContain").css("height", setheight);
+			$(".mainContain").css("padding-buttom", "50px");
+		}
 
 		if (width != ori_width) {
 			if (width > 767) {
