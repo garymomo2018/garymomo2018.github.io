8b4f297b35a48f66c0b3e8c76c6d0cd4c39f1cf1
garymomo2018 <garymomo2018@gmail.com>
2018-08-31 18:27:18 +0800

    2018/08/31 音樂作品架構

--- js/general/custom/main-music.js
+++ js/general/custom/main-music.js
@@ -2,6 +2,7 @@
 	var width = window.innerWidth;
 	var ori_width = "0";
 	var setheight = "0";
+	var animationEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
 
 	resize();
 	$(window).on("resize", resize);
@@ -19,13 +20,51 @@
 			if (width > 767) {
 				$(".container-fluid").css("padding-right", "0px");
 				$(".container-fluid").css("padding-left", "0px");
-				$("#arrowBody").css("height", $(".actions").css("height"));
+				$(".music-intro-dummy-1").show();
+				$(".music-intro-dummy-2").show();
 			} else {
 				$(".container-fluid").css("padding-right", "15px");
 				$(".container-fluid").css("padding-left", "15px");
-				$("#arrowBody").css("height", $(".actions").css("height"));
+				$(".music-intro-dummy-1").hide();
+				$(".music-intro-dummy-2").hide();
 			}
 		}
 		ori_width = width;
 	}
+
+	var fontTL = new TimelineMax({repeat: -1});
+	var fTLTime = 8;
+
+	fontTL.to($(".colorControl"), fTLTime, {
+		css : {color : "#f596aa", borderColor: "#f596aa"},
+		ease:Back.easeOut
+	})
+	.to($(".colorControl"), fTLTime, {
+		css : {color : "#ffb11b", borderColor: "#ffb11b"},
+		ease:Back.easeOut
+	})
+	.to($(".colorControl"), fTLTime, {
+		css : {color : "#e83015", borderColor: "#e83015"},
+		ease:Back.easeOut
+	})
+	.to($(".colorControl"), fTLTime, {
+		css : {color : "#986db2", borderColor: "#986db2"},
+		ease:Back.easeOut
+	});
+
+	$(".music-cover").on("click tap", function() {
+		$(this).addClass("animated zoomOutLeft").one(animationEnd, function() {
+			$(this).hide();
+			var target = $(this).parents(".music-items").find(".music-intro-1");
+			target.show();
+			target.addClass("animated fadeInLeft");
+			target = $(this).parents(".music-items").find(".music-intro-2");
+			target.show();
+			target.addClass("animated fadeInLeft");
+			target = $(this).parents(".music-items").find(".music-intro-3");
+			target.show();
+			target.addClass("animated fadeInRight");
+			location.hash = "#" + $(this).parents(".music-items").attr("id");
+		});
+	});
 });
